f91d8bc655aacbbc55f80f5387b08498bb0cde6a
joeshu <350027830@qq.com>
2026-03-26 09:36:04 +0800

    Update logger.js

--- src/core/logger.js
+++ src/core/logger.js
@@ -1,1 +1,25 @@
 
+const Logger = (() => {
+  const isDebug = false;
+  
+  if (!isDebug) {
+    return {
+      info: () => {},
+      error: () => {},
+      debug: () => {},
+      warn: () => {}
+    };
+  }
+
+  const now = () => {
+    const d = new Date();
+    return `${String(d.getHours()).padStart(2,'0')}:${String(d.getMinutes()).padStart(2,'0')}:${String(d.getSeconds()).padStart(2,'0')}`;
+  };
+
+  return {
+    info: (tag, msg) => console.log(`[UnifiedVIP][${now()}][INFO][${tag}] ${msg}`),
+    error: (tag, msg) => console.log(`[UnifiedVIP][${now()}][ERROR][${tag}] ${msg}`),
+    debug: (tag, msg) => console.log(`[UnifiedVIP][${now()}][DEBUG][${tag}] ${msg}`),
+    warn: (tag, msg) => console.log(`[UnifiedVIP][${now()}][WARN][${tag}] ${msg}`)
+  };
+})();
