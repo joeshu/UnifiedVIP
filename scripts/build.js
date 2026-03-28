ac44dbf5bf25d496dd30bddf49ce43f0c7f752c8
joeshu <350027830@qq.com>
2026-03-28 22:11:56 +0800

    更新 build.js

--- scripts/build.js
+++ scripts/build.js
@@ -14,7 +14,7 @@ const BuildGenerators = require('./build/generators');
 const BUILD_CONFIG = {
   // 手动开关：true=开启，false=关闭
   ENABLE_DIAGNOSE: false,
-  DEBUG_MODE: false,
+  DEBUG_MODE: true,
 
   // 可选手动版本后缀，例如 'lazy' / 'beta'
   // 留空 '' 则使用 package.json.version 原始版本
